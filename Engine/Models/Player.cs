31f7b2502da094a45378372c0476f71f10dffc58
Michael Baker <michael.a.baker34@gmail.com>
2021-09-16 23:11:14 -0500

    add properties to player class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -8,5 +8,11 @@ namespace Engine.Models
 {
     class Player
     {
+        string Name { get; set; }
+        string Class { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
     }
 }
