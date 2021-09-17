15fb938830e806c63ca39d3d15b5b639f0e24a33
Michael Baker <michael.a.baker34@gmail.com>
2021-09-16 23:22:55 -0500

    add viewmodels directory. create gamesession class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        // GameSession Object Constructor
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+        }
+    }
+    
+}
