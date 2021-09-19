4942dec770e705173b713d091feac34b5accd931
Michael Baker <michael.a.baker34@gmail.com>
2021-09-18 23:30:09 -0500

    add  private variables for player properties. refactor get/set to include private vars and event listeners

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -7,13 +7,46 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
+    // implements NotifyPropertyChanged interface for OnPropertyChanged event listener
     public class Player : INotifyPropertyChanged
     {
+        private string _name;
+        private string _class;
+        private int _hitPoints;
         private int _experiencePoints;
+        private int _level;
+        private int _gold;
 
-        public string Name { get; set; }
-        public string Class { get; set; }
-        public int HitPoints { get; set; }
+        public string Name 
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            } 
+        }
+
+        public string Class 
+        {
+            get { return _class; } 
+            set
+            {
+                _class = value;
+                OnPropertyChanged("Class");
+            } 
+        }
+
+        public int HitPoints 
+        { 
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+
         public int ExperiencePoints 
         {
             get { return _experiencePoints; }
@@ -23,8 +56,26 @@ namespace Engine.Models
                 OnPropertyChanged("ExperiencePoints");
             } 
         }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+
+        public int Level 
+        { 
+            get { return _level; } 
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        
+        }
+        public int Gold 
+        { 
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            } 
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
