d5e90a5b0d861e0993a67b88d1b52b7aefa3e8cc
Michael Baker <michael.a.baker34@gmail.com>
2021-09-18 22:14:37 -0500

    add experience button to mainwindow view add experience method to mainwindow gamesession

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -32,5 +32,10 @@ namespace WPFUI
             // DataContext properties for MainWindow.xaml
             DataContext = _gameSession;
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
     }
 }
