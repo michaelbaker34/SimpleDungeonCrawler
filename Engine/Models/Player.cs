74de736b7700b22c8634d66e34296fe80d0a0a77
Michael Baker <michael.a.baker34@gmail.com>
2021-09-18 23:17:12 -0500

    create onpropertychanged method for player class. refactor experiencepoints get/set method to change with button click

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
+        private int _experiencePoints;
+
         public string Name { get; set; }
         public string Class { get; set; }
         public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
+        public int ExperiencePoints 
+        {
+            get { return _experiencePoints; }
+            set 
+            { 
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            } 
+        }
         public int Level { get; set; }
         public int Gold { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
