c386c90704358d585678c59074f777f93fd033b5
Michael Baker <michael.a.baker34@gmail.com>
2021-09-23 23:16:44 -0500

    add some locations to world factory

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -13,8 +13,18 @@ namespace Engine.Factories
         {
             World newWorld = new World();
 
-            newWorld.AddLocation(0, -1, "Home", "No place like home.", "pack://application:,,,/Engine;component/Images/Locations/Home.png");
-            
+            newWorld.AddLocation(0, -1, "Home", 
+                "No place like home.", "pack://application:,,,/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(0, 0, "Village Center",
+                "A village center.", "");
+
+            newWorld.AddLocation(-1, 0, "Farm",
+                "A farm.", "");
+
+            newWorld.AddLocation(-1, -1, "Field",
+                "A field", "");
+
             return newWorld;
         }
     }
