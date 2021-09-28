1244bad487985567cae83b330023831de30f965c
Michael Baker <michael.a.baker34@gmail.com>
2021-09-27 19:41:41 -0500

    add location image files. add link to image for addlocation methods

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -18,25 +18,28 @@ namespace Engine.Factories
                 "No place like home.", "pack://application:,,,/Engine;component/Images/Locations/Home.png");
 
             newWorld.AddLocation(0, 0, "Village Center",
-                "A village center.", "");
+                "A village center.", "pack://application:,,,/Engine;component/Images/Locations/VillageCenter.png");
 
-            newWorld.AddLocation(-1, 0, "Farm",
-                "A farm.", "");
+            newWorld.AddLocation(-1, -1, "Farm",
+                "A farm.", "pack://application:,,,/Engine;component/Images/Locations/Farm.png");
 
-            newWorld.AddLocation(-1, -1, "Field",
-                "A field", "");
+            newWorld.AddLocation(-1, -2, "Field",
+                "A field", "pack://application:,,,/Engine;component/Images/Locations/Field.png");
 
-            newWorld.AddLocation(1, 0, "Tavern",
-                "A Tavern.", "");
+            newWorld.AddLocation(0, 1, "Tavern",
+                "A Tavern.", "pack://application:,,,/Engine;component/Images/Locations/Tavern.png");
 
-            newWorld.AddLocation(2, 0, "Cellar",
-                "A Cellar.", "");
+            newWorld.AddLocation(0, 2, "Cellar",
+                "A Cellar.", "pack://application:,,,/Engine;component/Images/Locations/Cellar.png");
 
-            newWorld.AddLocation(1, 0, "Front Gate",
-                "Village front gate", "");
+            newWorld.AddLocation(1 , 0, "Front Gate",
+                "Village front gate", "pack://application:,,,/Engine;component/Images/Locations/FrontGate.png");
 
-            newWorld.AddLocation(0, 1, "Forest",
-                "A Forest", "");
+            newWorld.AddLocation(2, 0, "Forest",
+                "A Forest", "pack://application:,,,/Engine;component/Images/Locations/Forest.png");
+
+            newWorld.AddLocation(-1 , 0, "Trader",
+                "Trader", "pack://application:,,,/Engine;component/Images/Locations/Trader.png");
 
             return newWorld;
         }
